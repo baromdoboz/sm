035dbba4518a4eaa2fa4391222e410674e7070f3
baromdoboz <baromdoboz@gmail.com>
2025-03-15 23:06:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const text = spacematic.textContent
 spacematic.textContent = ""
 
 function createLetters() {
-  // Töröljük a korábbi betűket
   spacematic.innerHTML = ""
 
-  spacematic.style.animation = "none" // Az animáció újraindítása
-  spacematic.offsetHeight // Force reflow (újraszámolás), hogy az animáció újrainduljon
-  spacematic.style.animation = "" // Az animációs stílus újra alkalmazása
+  spacematic.style.animation = "none"
+  spacematic.offsetHeight
+  spacematic.style.animation = ""
 
-  // Létrehozzuk és animáljuk a betűket
   for (let i = 0; i < text.length; i++) {
     const letter = document.createElement("span")
     letter.classList.add("letter")
@@ -24,31 +22,12 @@ function createLetters() {
     letter.style.animationDelay = `${i * 0.2}s`
     spacematic.appendChild(letter)
   }
-
-  // Visszaállítjuk a div opacity-ját 1-re és újraindítjuk az animációt
-  //  spacematic.style.opacity = 1;
-  //  spacematic.style.animation = 'none'; // Az animáció újraindítása
-  //  spacematic.offsetHeight; // Force reflow (újraszámolás), hogy az animáció újrainduljon
-  //  spacematic.style.animation = ''; // Az animációs stílus újra alkalmazása
 }
 
-// A kattintás esemény figyelése, és a createLetters hívása minden kattintáskor
 document.addEventListener("click", function () {
-  //  spacematic.style.opacity = 1;
-  /*
-  spacematic.style.animation = 'none'; // Az animáció újraindítása
-  spacematic.offsetHeight; // Force reflow (újraszámolás), hogy az animáció újrainduljon
-  spacematic.style.animation = ''; // Az animációs stílus újra alkalmazása
-*/
-  // Visszaállítjuk az opacity-t, hogy újra látható legyen
-  //  spacematic.style.opacity = 1;
-
-  // Indítjuk az animációt
-  //  setTimeout(createLetters, 1000)
   createLetters()
 })
 
-// A createLetters függvényt lefuttatjuk 1 másodperces késleltetéssel az oldal betöltődésekor
 window.addEventListener("load", function () {
-  setTimeout(createLetters, 1000) // 1000 ms = 1 másodperc késleltetés
+  setTimeout(createLetters, 1000)
 })
