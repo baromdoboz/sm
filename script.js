05fc1e00f8b63ffe23c4acd5dd9489b290cb26a4
baromdoboz <baromdoboz@gmail.com>
2025-03-15 21:24:14 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const spacematic = document.getElementById('spacematic');
+const text = spacematic.textContent;
+
+spacematic.textContent = '';
+
+function createLetters() {
+  for (let i = 0; i < text.length; i++) {
+    const letter = document.createElement('span');
+    letter.classList.add('letter');
+    letter.textContent = text[i];
+
+    if (text[i] === 'S' || text[i] === 'M') {
+      letter.classList.add('lightblue');
+    } else {
+      letter.classList.add('black');
+    }
+
+    letter.style.animationDelay = `${i * 0.2}s`;
+    spacematic.appendChild(letter);
+  }
+}
+
+setTimeout(createLetters, 500);
